2d70f1ed259b00f25fe8f1efa66b537bb4bac55e
gundo94 <dhkwon8@naver.com>
2024-03-25 16:31:20 +0900

    C:/Program Files/Git/usr/article/write라고 입력하면 게시물 제목, 내용 입력받기

--- src/main/java/com/sbs/exam/board/Main.java
+++ src/main/java/com/sbs/exam/board/Main.java
@@ -12,8 +12,19 @@ public class Main {
     while (true) {
       System.out.printf("명령) ");
       String cmd = sc.nextLine();
+      if (cmd.equals("/usr/article/write")) {
+        System.out.println("== 게시물 작성 ==");
+        System.out.printf("제목 : " );
+        String title = sc.nextLine();
 
-      if (cmd.equals("exit")) {
+        System.out.printf("내용 : ");
+        String body = sc.nextLine();
+
+        int id = 1;
+        System.out.printf("%d번 게시물이 생성되었습니다.\n", id);
+      }
+
+      else if (cmd.equals("exit")) {
         System.out.println("프로그램을 종료합니다.");
         break;
       }
