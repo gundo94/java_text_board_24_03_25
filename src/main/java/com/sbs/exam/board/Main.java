b3ae8b72b7fd8e031395da6684fbe00d21085bd1
gundo94 <dhkwon8@naver.com>
2024-03-26 10:50:05 +0900

    aaa

--- src/main/java/com/sbs/exam/board/Main.java
+++ src/main/java/com/sbs/exam/board/Main.java
@@ -1,44 +1,71 @@
 package com.sbs.exam.board;
 
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+
+  static void makeTestData(List<Article> articles){
+    articles.add(new Article(1, "제목1", "내용1"));
+    articles.add(new Article(2, "제목2", "내용2"));
+    articles.add(new Article(3, "제목3", "내용3"));
+
+  }
   public static void main(String[] args) {
     System.out.println("== 자바 텍스트 게시판 0.1v ==");
     System.out.println("== 자바 텍스트 게시판 시작 ==");
 
     Scanner sc = new Scanner(System.in);
     int articleLastId = 0;
-    Article lastArticle = null;
+
+    List<Article> articles = new ArrayList<>();
+    makeTestData(articles);
+
+    if(articles.size()>0){
+      articleLastId = articles.get(articles.size()-1).id;
+    }
 
     while (true) {
       System.out.printf("명령) ");
       String cmd = sc.nextLine();
       if (cmd.equals("/usr/article/write")) {
         System.out.println("== 게시물 작성 ==");
-        System.out.printf("제목 : " );
+        System.out.printf("제목 : ");
         String title = sc.nextLine();
 
         System.out.printf("내용 : ");
         String body = sc.nextLine();
 
-        int id = ++articleLastId ;
+        int id = ++articleLastId;
 
-        Article article = new Article(id,title, body);
-        lastArticle = article;
+        Article article = new Article(id, title, body);
 
-        System.out.println("생성 된 게시물 객체 : " + article);
+
+        articles.add(article);
         System.out.printf("%d번 게시물이 생성되었습니다.\n", id);
+      }
+      else if (cmd.equals("/usr/article/list")) {
+        System.out.println("== 게시물 리스트 ==");
+        System.out.println("=================");
+        System.out.println("번호 / 제목");
+        System.out.println("=================");
+
+        for (int i = articles.size()-1 ; i >=0 ;i--) {
+          Article article = articles.get(i);
+          System.out.printf("%d / %s\n", article.id, article.title);
+        }
       }
 
       else if (cmd.equals("/usr/article/detail")) {
 
-        if(lastArticle == null) {
+        Article article = articles.get(articles.size()-1);
+
+        if(articles.isEmpty()) {
           System.out.println("게시물이 존재하지 않습니다.");
           continue;
         }
-
-        Article article = lastArticle;
 
         System.out.println("== 게시물 상세내용 ==");
         System.out.printf("번호 : %d\n", article.id);
